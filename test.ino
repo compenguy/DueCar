dd92913e881c061052350c76cf0013439f48069b
Will Page <compenguy@gmail.com>
2021-04-30 20:18:52 -0700

    clang-format and minor code cleanup

--- test.ino
+++ test.ino
@@ -1,4 +1,4 @@
-// USBHost - Version: Latest 
+// USBHost - Version: Latest
 
 #include "GamepadController.h"
 #include <cstdio>
@@ -6,7 +6,6 @@
 /*
 
 */
-
 
 // Initialize USB Controller
 USBHost usb;
@@ -18,31 +17,24 @@ char outbuf[64];
 
 // This function intercepts unknown gamepad reports
 void unhandledInput() {
-  uint8_t packet[8];
-  
-  if (gamepad.getLastBuf(packet)) {
-    snprintf(outbuf,
-             sizeof(outbuf),
-             "Gamepad packet: %02x %02x %02x %02x %02x %02x %02x %02x",
-             (int)packet[0],
-             (int)packet[1],
-             (int)packet[2],
-             (int)packet[3],
-             (int)packet[4],
-             (int)packet[5],
-             (int)packet[6],
-             (int)packet[7]);
-    Serial.println((char*)outbuf);
-  }
+    uint8_t packet[8];
+
+    if (gamepad.getLastBuf(packet)) {
+        Serial.print("Gamepad packet: ");
+        for (int i = 0; i < sizeof(packet); i++) {
+            Serial.print(packet[i], HEX);
+        }
+        Serial.println();
+    }
 };
 
 void setup() {
-  Serial.begin(9600);
-  Serial.println("Program started");
-  delay(200);
+    Serial.begin(9600);
+    Serial.println("Program started");
+    delay(200);
 }
 
 void loop() {
-  // Process USB tasks
-  usb.Task();
+    // Process USB tasks
+    usb.Task();
 }
